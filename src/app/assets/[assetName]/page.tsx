f8bd971e8489cf7e6dab36ffb1ea4dde6619f66f
Amir Basirati <amirbasirati13@gmail.com>
2024-03-07 19:11:19 +0330

    update: if responese was error and asset was not found redirects to 404 else the asset page renders 🔨

--- src/app/assets/[assetName]/page.tsx
+++ src/app/assets/[assetName]/page.tsx
@@ -1,5 +1,7 @@
 import AssetPage from "@/components/templates/asset/AssetPage";
 import TAssetItem from "@/types/assetItem";
+import assetCreator from "@/utils/helper/assetCreator";
+import { notFound } from "next/navigation";
 
 interface Props {
   params: { assetName: string };
@@ -13,29 +15,13 @@ const Asset: React.FC<Props> = async ({ params }) => {
   );
   const data = await res.json();
   //   console.log(data.tickers);
-  const assetItem: TAssetItem = {
-    name: data.name,
-    id: data.id,
-    symbol: data.symbol,
-    image: data.image,
-    marketCap: data.market_data.market_cap.usd,
-    marketCapRank: data.market_data.market_cap_rank,
-    description: data.description.en,
-    tickers: data.tickers,
-    priceChangePercent: {
-      _24h: data.market_data.price_change_percentage_24h,
-      _7d: data.market_data.price_change_percentage_7d,
-      _14d: data.market_data.price_change_percentage_14d,
-      _30d: data.market_data.price_change_percentage_30d,
-      _60d: data.market_data.price_change_percentage_60d,
-      _200d: data.market_data.price_change_percentage_200d,
-      _1y: data.market_data.price_change_percentage_1y,
-    },
-    currentPrice: data.market_data.current_price,
-    links: data.links,
-  };
+  if ("error" in data) {
+    notFound();
+  } else {
+    const assetItem: TAssetItem = assetCreator(data);
 
-  return <AssetPage asset={assetItem} />;
+    return <AssetPage asset={assetItem} />;
+  }
 };
 
 export default Asset;
