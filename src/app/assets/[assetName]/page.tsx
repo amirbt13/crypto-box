c30d3a8adb9af7ff06f791abef436bb81231fa35
Amir Basirati <amirbasirati13@gmail.com>
2024-03-07 20:02:45 +0330

    update: status 500 added to condition for redirecting to 404 page 🔨

--- src/app/assets/[assetName]/page.tsx
+++ src/app/assets/[assetName]/page.tsx
@@ -11,11 +11,12 @@ const Asset: React.FC<Props> = async ({ params }) => {
   const { assetName } = params;
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/coins/${assetName}`,
-    {}
+    { cache: "no-store" }
   );
+
   const data = await res.json();
   //   console.log(data.tickers);
-  if ("error" in data) {
+  if ("error" in data || res.status === 500) {
     notFound();
   } else {
     const assetItem: TAssetItem = assetCreator(data);
